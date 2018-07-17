4e14d42a23d390a712d0f7444b8cdc78ce050417
mizumasa <masaru0mizuochi@gmail.com>
2018-07-18 08:43:50 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+var count = 0;
+
+app.get('/api/hello', (req, res) => {
+  //res.send({ express: 'Hello From Express' });
+  count = count + 1;
+  res.send({ express: count});
+});
+
+
+var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
+var COMMENTS_FILE = path.join(__dirname, 'comments.json');
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Additional middleware which will set headers that we need on each request.
+app.use(function(req, res, next) {
+    // Set permissive CORS header - this allows this server to be used only as
+    // an API server in conjunction with something like webpack-dev-server.
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Disable caching so we'll always get the latest comments.
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+});
+
+app.get('/api/comments', function(req, res) {
+  fs.readFile(COMMENTS_FILE, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post('/api/comments', function(req, res) {
+  fs.readFile(COMMENTS_FILE, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    var comments = JSON.parse(data);
+    // NOTE: In a real implementation, we would likely rely on a database or
+    // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
+    // treat Date.now() as unique-enough for our purposes.
+    var newComment = {
+      id: Date.now(),
+      author: req.body.author,
+      text: req.body.text,
+    };
+    comments.push(newComment);
+    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      res.json(comments);
+    });
+  });
+});
+
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
